7bf0e527b5dae1edeebc6fc130c4aad843f4f0f3
TakashiRinkitori <minhhieupck1@gmail.com>
2024-10-28 09:49:04 +0700

    TK_Vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
+  <div id="app">
+    <Header />
+    <Body />
+    <!-- <Footer /> -->
+    <BootstrapFooter/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Body from './components/Body.vue';
+// import Footer from './components/Footer.vue';
+import BootstrapFooter from './components/BootstrapFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Body,
+    // Footer,
+    BootstrapFooter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/BootstrapFooter.vue
@@ -0,0 +1,43 @@
+<template>
+    <footer class="bg-dark text-white text-center py-4">
+        <a href="https://www.facebook.com" target="_blank" aria-label="Facebook" class="text-white mx-2">
+            <i class="fa fa-facebook-official"></i>
+        </a>
+        <a href="https://www.instagram.com" target="_blank" aria-label="Instagram" class="text-white mx-2">
+            <i class="fa fa-instagram"></i>
+        </a>
+        <a href="https://www.snapchat.com" target="_blank" aria-label="Snapchat" class="text-white mx-2">
+            <i class="fa fa-snapchat"></i>
+        </a>
+        <a href="https://www.pinterest.com" target="_blank" aria-label="Pinterest" class="text-white mx-2">
+            <i class="fa fa-pinterest-p"></i>
+        </a>
+        <a href="https://www.twitter.com" target="_blank" aria-label="Twitter" class="text-white mx-2">
+            <i class="fa fa-twitter"></i>
+        </a>
+        <a href="https://www.linkedin.com" target="_blank" aria-label="LinkedIn" class="text-white mx-2">
+            <i class="fa fa-linkedin"></i>
+        </a>
+        <p class="mt-3">
+            Copyright By <a href="https://getbootstrap.com" target="_blank" class="text-white"> Minh</a>
+        </p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'BootstrapFooter',
+};
+</script>
+
+<style scoped>
+footer {
+    position: relative;
+    bottom: 0;
+    width: 100%;
+}
+
+footer a:hover {
+    opacity: 0.7;
+}
+</style>
